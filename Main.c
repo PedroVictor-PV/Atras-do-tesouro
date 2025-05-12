e52dff5b3c1a5a4c78caac7c7f38d40d5c82fabd
Regis <regis.michael101@gmail.com>
2025-05-11 22:35:44 -0300

    Nova conversao de escolha

--- Main.c
+++ Main.c
@@ -2,109 +2,159 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
-#include <conio.h>
 
     // Adicionar socket
 
-
-    // Criar duas matrizes, uma abistraida e outra com as tesouros(pedro). 
-    // A matriz1[posicao][posicao] recebe matriz2[posicao][posicao] e verificar se o que 
-    // veio da segunda matriz é igual 'o';
-
-void mapeiaMatriz(char tabuleiro[4][4]){
-    for(int i=0; i<4; i++){
-        for(int j=0; j<4; j++){
+    //MAPEAR  A MATRIZ DE 1 AO FINAL DO TAMAHO
+    //ABSTRAIR A LOGICA PARA INFORMAR O NÚMERO DO QUADRA
+void mapeiaMatriz(int linha, int coluna, char tabuleiro[linha][coluna])
+{
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<4; j++)
+        {
             tabuleiro[i][j] = '-';
         }
     }
 }
 
-void embaralha(char tabuleiro[4][4]){
+void embaralha(int linha, int coluna, char tabuleiro[linha][coluna])
+{
     srand(time(NULL));
-    int linha = -1;
-    int coluna = -1;
+    int randLinha = -1;
+    int randColuna = -1;
     int qtd=0;
-    while(qtd<4){
-        linha = rand()%4;
-        coluna = rand()%4;
-            if(tabuleiro[linha][coluna] != 'o'){
-                tabuleiro[linha][coluna] = 'o';
-			    qtd++;
-            }
+    while(qtd<5)
+    {
+        randLinha = rand()%4;
+        randColuna = rand()%4;
+        if(tabuleiro[randLinha][randColuna] != 'o')
+        {
+            tabuleiro[randLinha][randColuna] = 'o';
+            qtd++;
+        }
     }
 }
 
-int main(int argc, char const *argv[]){
+char tradutorDeCoordenada(int numero, int linha, int coluna, char matriz[linha][coluna]) 
+{
+    int cont = 1;
+    char result = '?'; 
+
+    for (int i = 0; i < linha; i++)
+    {
+        for (int j = 0; j < coluna; j++)
+        {
+            if (numero == cont)
+            {
+                return matriz[i][j];
+            }
+            cont++;
+        }
+    }
+
+    return result;
+}
+
+
+int main(int argc, char const *argv[])
+{
+
+// -------------------------------------------------------------------------
     char tabuleiroPlayer[4][4];
     char tabuleiroTesouro[4][4];
+    int escolha;
 
-    int pontos= 0, tempo= 4;
+    mapeiaMatriz(4, 4, tabuleiroPlayer);
+    mapeiaMatriz(4, 4, tabuleiroTesouro);
+    embaralha(4, 4, tabuleiroTesouro);
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            printf(" %c", tabuleiroTesouro[i][j]);       
+        }
+        printf("\n");
+    }
+    printf("Escolha: [1 | 16]");
+    scanf("%d", &escolha);
 
-    printf("\n======GO TO TREASURE!======\n");
+
+
+    char a = tradutorDeCoordenada(escolha, 4, 4, tabuleiroPlayer);
+    char b = tradutorDeCoordenada(escolha, 4, 4, tabuleiroTesouro);
+
+    printf(" %c", a);
+    printf(" %c", b);
+
+// -------------------------------------------------------------------------
+
+    // int pontos= 0, tentativas = 10;
+
+    // printf("\n======GO TO TREASURE!======\n");
     
-    mapeiaMatriz(tabuleiroPlayer);
-    mapeiaMatriz(tabuleiroTesouro);
-    embaralha(tabuleiroTesouro);
+    // //CRIAR UMA FUNÇAO PARA REDUZIR O CHAMAMENTO DUPLO
+    // mapeiaMatriz(tabuleiroPlayer);
+    // mapeiaMatriz(tabuleiroTesouro);
+    // embaralha(tabuleiroTesouro);
 
-    int linhaJog= -1, colunaJog= -1;
+    // int linhaJog= -1, colunaJog= -1;
+    
+    // do
+    // {   
+    //     printf("\n%d Tentativas restantes!", tentativas);
+    //     printf("\nPONTOS: %d", pontos);
+    //     printf("\nEscolha a linha [0-3]: ");
+    //     scanf("%d", &linhaJog);
+    //     printf("\nEscolha a coluna [0-3]: ");
+    //     scanf("%d", &colunaJog);
 
-    char cont = 'n';
-    do
-    {   
-        printf("\nPONTOS: %d", pontos);
-        printf("\nEscolha a linha [0-3]: ");
-        scanf("%d", &linhaJog);
-        printf("\nEscolha a coluna [0-3]: ");
-        scanf("%d", &colunaJog);
+    //     if(linhaJog > 3 || colunaJog > 3)
+    //     {
+    //         printf("\nValor invalido!!");
+    //         printf("\nEscolha novamente a linha [0-3]: ");
+    //         scanf("%d", &linhaJog);
+    //         printf("\nEscolha novamente a coluna [0-3]: ");
+    //         scanf("%d", &colunaJog);
+    //     }
 
-        if(linhaJog > 3 || colunaJog > 3)
-        {
-            printf("\nValor invalido!!");
-            printf("\nEscolha novamente a linha [0-3]: ");
-            scanf("%d", &linhaJog);
-            printf("\nEscolha novamente a coluna [0-3]: ");
-            scanf("%d", &colunaJog);
-        }
+    //     tabuleiroPlayer[linhaJog][colunaJog] = tabuleiroTesouro[linhaJog][colunaJog];
 
-        tabuleiroPlayer[linhaJog][colunaJog] = tabuleiroTesouro[linhaJog][colunaJog];
-        if(tabuleiroPlayer[linhaJog][colunaJog] == 'o')
-        {
-            tabuleiroPlayer[linhaJog][colunaJog] = '$';
-            pontos+=1;
-                for(int i =0; i<4; i++)
-                {
-                    for(int j=0; j<4; j++)
-                    {
-                        printf(" %c", tabuleiroPlayer[i][j]);
-                    }
-                    printf("\n");
+    //     if(tabuleiroPlayer[linhaJog][colunaJog] == 'o')
+    //     {
+    //         tabuleiroPlayer[linhaJog][colunaJog] = '$';
+    //         pontos+=1;
+    //             for(int i =0; i<4; i++)
+    //             {
+    //                 for(int j=0; j<4; j++)
+    //                 {
+    //                     printf(" %c", tabuleiroPlayer[i][j]);
+    //                 }
+    //                 printf("\n");
                     
-                }
-                    mapeiaMatriz(tabuleiroPlayer);
-                    mapeiaMatriz(tabuleiroTesouro);
-                    embaralha(tabuleiroTesouro);
+    //             }
 
-                 
-        }else
-        {
-            tabuleiroPlayer[linhaJog][colunaJog] = '*';
-            for(int i =0; i<4; i++)
-            {
-                for(int j=0; j<4; j++)
-                {
-                    printf(" %c", tabuleiroPlayer[i][j]);
-                }
-                printf("\n");
-            }
-        }
+    //                 mapeiaMatriz(tabuleiroPlayer);
+    //                 mapeiaMatriz(tabuleiroTesouro);
+    //                 embaralha(tabuleiroTesouro);
 
-        // Trocar por tentativas
+    //     }else
+    //     {
+    //         tabuleiroPlayer[linhaJog][colunaJog] = '*';
+    //         for(int i =0; i<4; i++)
+    //         {
+    //             for(int j=0; j<4; j++)
+    //             {
+    //                 printf(" %c", tabuleiroPlayer[i][j]);
+    //             }
+    //             printf("\n");
+    //         }
+    //     }
+    //     tentativas-=1;
+        
+    // }while(tentativas > 0);
 
-        printf("\nCONTINUE?[s/n]");
-        scanf(" %c", &cont);
-    }while(cont== 's');
-
-    printf("\nPONTOS: %d", pontos);
+    // printf("\nPONTOS: %d", pontos);
 
     return 0;
 }
