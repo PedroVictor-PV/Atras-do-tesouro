d98d4a8d773ef5efdeb8201399a99c5f757c0c14
PedroVictor-PV <pedro.cavalcante0515@gmail.com>
2025-05-24 18:33:12 -0300

    atualizando cabecalho funcoes jogo

--- include/jogo.h
+++ include/jogo.h
@@ -1,11 +1,16 @@
-#include <windows.h>  // Para GetCurrentThreadId
-
 #ifndef JOGO_H
 #define JOGO_H
 
+#include <winsock2.h>  
+#include <windows.h> 
+
 #define LINHAS 6
 #define COLUNAS 6
+#define LIMITE_CLIENTES 20
 
+extern SOCKET listaClientes[LIMITE_CLIENTES];
+extern CRITICAL_SECTION csListaClientes;
+extern volatile LONG clientesConectados;
 typedef struct 
 {
     char nome[110];
@@ -13,10 +18,12 @@ typedef struct
 } Jogador;
 
 
-
 void inicializaMatriz(char tabuleiro[LINHAS][COLUNAS]);
 void embaralhaTesouros(char tabuleiro[LINHAS][COLUNAS]);
 int tradutorDeCoordenada(int pos, char tabuleiro[LINHAS][COLUNAS], char tabuleiro2[LINHAS][COLUNAS]);
 void matrizParaTexto(char tabuleiro[LINHAS][COLUNAS], char *saida);
+
+void adicionarCliente(SOCKET cliente);
+void removerCliente(SOCKET cliente);
 
 #endif
