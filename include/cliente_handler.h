55eda8d02f0201b7d405cf29bdec6fe3817a312e
PedroVictor-PV <pedro.cavalcante0515@gmail.com>
2025-05-24 18:32:44 -0300

    adicionando funcoes no header

--- include/cliente_handler.h
+++ include/cliente_handler.h
@@ -1,8 +1,12 @@
 #ifndef CLIENTE_HANDLER_H
 #define CLIENTE_HANDLER_H
 
+
 #include <winsock2.h>
 #include <windows.h>
+#define LIMITE_CLIENTES 20
+extern SOCKET listaClientes[LIMITE_CLIENTES];
+extern CRITICAL_SECTION csListaClientes;
 
 DWORD WINAPI threadCliente(LPVOID param);
 
@@ -10,5 +14,9 @@ typedef struct
 {
     SOCKET cliente;
 } ThreadArgs;
+
+void adicionarCliente(SOCKET cliente);
+void removerCliente(SOCKET cliente);
+void broadcastAtualizacao();
 
 #endif
